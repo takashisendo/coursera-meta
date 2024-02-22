d6c5191d152e0dd1d03d0d7c47e97d19c8f49bd2
Takashi Sendo <takashi.sendo@gmail.com>
2024-02-22 13:45:12 +0900

    Commit directory.

---
+++ Meta iOS Developer Professional Certificate/#3 Programming Fundamentals in Swift/W1/First.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let day =  "Monday";
+let dailyTemperature = 75
+print("Today is \(day)")
+print("The temperature on \(day) is \(dailyTemperature)°F")
+var temperature = 70
+print("The temperature on Monday morning is 70°F.")
+temperature  =  80
+print("The temperature on Monday evening is 80°F.")
+let weeklyTemperature = 75
+temperature = weeklyTemperature
+print("The average temperature this week is 75°F.")
