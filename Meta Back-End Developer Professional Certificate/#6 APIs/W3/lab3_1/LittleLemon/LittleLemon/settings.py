99c2b3ffe42f1649c4baa15235f0149c077e6b3e
Takashi Sendo <takashi.sendo@gmail.com>
2024-02-27 19:53:51 +0900

    Add files for W3.

--- Meta Back-End Developer Professional Certificate/#6 APIs/W3/lab3_1/LittleLemon/LittleLemon/settings.py
+++ Meta Back-End Developer Professional Certificate/#6 APIs/W3/lab3_1/LittleLemon/LittleLemon/settings.py
@@ -125,5 +125,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    # ENTER YOUR CODE HERE
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3
 }
