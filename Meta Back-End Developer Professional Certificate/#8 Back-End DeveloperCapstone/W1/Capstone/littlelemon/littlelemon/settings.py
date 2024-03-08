ab99d2925a4c0c092d073d4594c061c7146fddff
Takashi Sendo <takashi.sendo@gmail.com>
2024-03-08 20:01:26 +0900

    Att littlelemon files.

--- Meta Back-End Developer Professional Certificate/#8 Back-End DeveloperCapstone/W1/Capstone/littlelemon/littlelemon/settings.py
+++ Meta Back-End Developer Professional Certificate/#8 Back-End DeveloperCapstone/W1/Capstone/littlelemon/littlelemon/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,9 +77,16 @@ WSGI_APPLICATION = "littlelemon.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'USER' : 'root',
+        'PASSWORD' : '##TsTs2968',
+        'OPTIONS':{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
