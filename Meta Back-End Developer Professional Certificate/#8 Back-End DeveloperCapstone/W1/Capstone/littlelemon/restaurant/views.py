61fe2ddcaad5dedd37f8fe3d90fcc6e57a6d2952
Takashi Sendo <takashi.sendo@gmail.com>
2024-03-11 23:34:11 +0900

    Adding a texg_models.

--- Meta Back-End Developer Professional Certificate/#8 Back-End DeveloperCapstone/W1/Capstone/littlelemon/restaurant/views.py
+++ Meta Back-End Developer Professional Certificate/#8 Back-End DeveloperCapstone/W1/Capstone/littlelemon/restaurant/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from .models import Menu
+from .models import Booking
+from .models import MenuItem
 from .serializers import MenuSerializer
 from rest_framework import viewsets
-from .models import Booking
+from .serializers import MenuItemSerializer
 from .serializers import BookingSerializer
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from .serializers import UserSerializer
 
 # Create your views here.
@@ -15,18 +18,19 @@ def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [AllowAny]
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [AllowAny]
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()  # Fetches all Booking objects
     serializer_class = BookingSerializer  # Specifies to use BookingSerializer for serialization
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
