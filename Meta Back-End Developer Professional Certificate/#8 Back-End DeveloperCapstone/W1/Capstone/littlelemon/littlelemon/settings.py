66f17c88d1b0ec35a8c967b1edb2b637c4e52942
Takashi Sendo <takashi.sendo@gmail.com>
2024-03-12 11:06:05 +0900

    Modify setting.py.

--- Meta Back-End Developer Professional Certificate/#8 Back-End DeveloperCapstone/W1/Capstone/littlelemon/littlelemon/settings.py
+++ Meta Back-End Developer Professional Certificate/#8 Back-End DeveloperCapstone/W1/Capstone/littlelemon/littlelemon/settings.py
@@ -12,10 +12,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import pymysql
+import os
+
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Quick-start development settings - unsuitable for production
